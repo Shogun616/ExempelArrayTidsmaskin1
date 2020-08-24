2efb0c774ff448cd9446db6f931d617b23f4ea3c
Philip Mattsson <philip.mattsson@iths.se>
2020-08-24 11:27:53 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] musik = {"The Beatle ger ut Abbey Road!", "Jimi Hendrix spelar på Woodstock 69!",
+            "Frank Zappa spelar med Mothers i Göteborg", "Grateful Dead släpper Live Dead!",
+    "Black Sabbath släpper Paranoid!"};
+
+	String[] sport = {"Fotbolls-VM!", "Ishockey-VM!", "OS i London!", "OS i Tokyo!", "Formel-1 i New Dehli!"};
+
+
+    }
+}
